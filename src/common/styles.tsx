2f46d066e9e6934f2fe4e043c2a534e3c0ccc039
Status100 <bluemagicuk@yahoo.co.uk>
2021-06-06 03:15:14 +0100

    set standard text size to 14px

--- src/common/styles.tsx
+++ src/common/styles.tsx
@@ -3,9 +3,7 @@ import styled from "styled-components";
 export const PageContainer = styled.div`
   position:relative;
   height: 100vh;
-  border:2px solid red;
-    overflow-y:scroll;
-    display: flex;
-    flex-flow: column;
-
+  overflow-y:scroll;
+  display: flex;
+  flex-flow: column;
 `;

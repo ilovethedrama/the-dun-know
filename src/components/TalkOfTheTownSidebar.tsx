cb7aca0cacee1cb552c2799bdaf6ee8c715bf6c5
Status100 <bluemagicuk@yahoo.co.uk>
2021-06-06 03:16:45 +0100

    Create highlight articles component

---
+++ src/components/TalkOfTheTownSidebar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+interface Props { }
+
+const TalkOfTheTownSidebar: React.FC<Props> = (props) => {
+
+  const TalkOfTheTownList = [
+    {
+      name: "",
+      link: ""
+    },
+    {
+      name: "",
+      link: ""
+    },
+    {
+      name: "",
+      link: ""
+    },
+    {
+      name: "",
+      link: ""
+    }];
+
+  return (
+    <>
+      <div>TalkOfTheTownSidebar content</div>
+    </>
+  )
+}
+
+export default TalkOfTheTownSidebar;
---
+++ src/components/TestArticle.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+import TheDunKnowLogo from "./TheDunKnowLogo";
+
+interface Props { }
+
+const Container = styled.div`
+  div {
+    padding: 20px 0;
+  }
+`;
+
+
+const TestArticle: React.FC<Props> = () => {
+  return (
+    <>
+      <Container>
+        <TheDunKnowLogo size={"large"} />
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores neque laborum ut animi molestiae aspernatur harum, ipsam dolor consequatur dolore, nobis cupiditate aliquid distinctio ab vel exercitationem dolorem velit voluptatem.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores neque laborum ut animi molestiae aspernatur harum, ipsam dolor consequatur dolore, nobis cupiditate aliquid distinctio ab vel exercitationem dolorem velit voluptatem.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores neque laborum ut animi molestiae aspernatur harum, ipsam dolor consequatur dolore, nobis cupiditate aliquid distinctio ab vel exercitationem dolorem velit voluptatem.
+      </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores neque laborum ut animi molestiae aspernatur harum, ipsam dolor consequatur dolore, nobis cupiditate aliquid distinctio ab vel exercitationem dolorem velit voluptatem.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores neque laborum ut animi molestiae aspernatur harum, ipsam dolor consequatur dolore, nobis cupiditate aliquid distinctio ab vel exercitationem dolorem velit voluptatem.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores neque laborum ut animi molestiae aspernatur harum, ipsam dolor consequatur dolore, nobis cupiditate aliquid distinctio ab vel exercitationem dolorem velit voluptatem.
+      </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores neque laborum ut animi molestiae aspernatur harum, ipsam dolor consequatur dolore, nobis cupiditate aliquid distinctio ab vel exercitationem dolorem velit voluptatem.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores neque laborum ut animi molestiae aspernatur harum, ipsam dolor consequatur dolore, nobis cupiditate aliquid distinctio ab vel exercitationem dolorem velit voluptatem.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores neque laborum ut animi molestiae aspernatur harum, ipsam dolor consequatur dolore, nobis cupiditate aliquid distinctio ab vel exercitationem dolorem velit voluptatem.
+      </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores neque laborum ut animi molestiae aspernatur harum, ipsam dolor consequatur dolore, nobis cupiditate aliquid distinctio ab vel exercitationem dolorem velit voluptatem.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores neque laborum ut animi molestiae aspernatur harum, ipsam dolor consequatur dolore, nobis cupiditate aliquid distinctio ab vel exercitationem dolorem velit voluptatem.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores neque laborum ut animi molestiae aspernatur harum, ipsam dolor consequatur dolore, nobis cupiditate aliquid distinctio ab vel exercitationem dolorem velit voluptatem.
+      </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores neque laborum ut animi molestiae aspernatur harum, ipsam dolor consequatur dolore, nobis cupiditate aliquid distinctio ab vel exercitationem dolorem velit voluptatem.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores neque laborum ut animi molestiae aspernatur harum, ipsam dolor consequatur dolore, nobis cupiditate aliquid distinctio ab vel exercitationem dolorem velit voluptatem.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores neque laborum ut animi molestiae aspernatur harum, ipsam dolor consequatur dolore, nobis cupiditate aliquid distinctio ab vel exercitationem dolorem velit voluptatem.
+      </div>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores neque laborum ut animi molestiae aspernatur harum, ipsam dolor consequatur dolore, nobis cupiditate aliquid distinctio ab vel exercitationem dolorem velit voluptatem.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores neque laborum ut animi molestiae aspernatur harum, ipsam dolor consequatur dolore, nobis cupiditate aliquid distinctio ab vel exercitationem dolorem velit voluptatem.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores neque laborum ut animi molestiae aspernatur harum, ipsam dolor consequatur dolore, nobis cupiditate aliquid distinctio ab vel exercitationem dolorem velit voluptatem.
+      </div>
+      </Container>
+    </>
+
+  )
+}
+
+export default TestArticle;

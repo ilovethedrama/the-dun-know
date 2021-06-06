8d8c3ea37c1f30ebd4dd9eb3efbab948c71b6e26
Status100 <bluemagicuk@yahoo.co.uk>
2021-06-06 01:31:19 +0100

    Make page content expand available  screen space and pass screensize prop to navbar

--- src/views/LandingScreen.tsx
+++ src/views/LandingScreen.tsx
@@ -1,15 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "./../components/Footer";
 import Navbar from "./../components/Navbar";
-import { PageContainer } from "./../common/styles"
+import { PageContainer } from "./../common/styles";
+import { useWindowSize } from "../hooks/hooks";
+import styled from "styled-components";
+import TestArticle from "../components/TestArticle";
 
 interface Props { }
+const PageContent = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
 
 const LandingScreen: React.FC<Props> = () => {
+  const [screenSize, setScreenSize] = useState<"MOBILE" | "TABLET" | "DESKTOP">("DESKTOP")
+  const size = useWindowSize();
+
+  useEffect(() => {
+    if (size.width < 1000) {
+      setScreenSize("TABLET")
+    } else {
+      setScreenSize("DESKTOP")
+    }
+  }, [size.width])
+
   return (
     <PageContainer>
-      <Navbar />
-      <div>The home page bruh!</div>
+      <Navbar screenSize={screenSize} />
+      <PageContent>
+        The home page bruh!
+        <TestArticle></TestArticle>
+      </PageContent>
       <Footer />
     </PageContainer>
   )

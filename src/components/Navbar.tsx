4750da2b83856f17886d6b5a7e7f16f100c50e71
Status100 <bluemagicuk@yahoo.co.uk>
2021-06-06 02:09:41 +0100

    Add styling and menu options

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,58 @@
 import React from "react";
 import TheDunKnowLogo from "./TheDunKnowLogo";
 
-interface Props { }
+import styled from "styled-components";
 
-const Navbar: React.FC<Props> = () => {
+interface Props {
+  screenSize: string
+}
+
+const NavContainer = styled.div`
+  font-family: "Playfair Display SC",serif;
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;  
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  /* background: black;
+  color: white; */
+`;
+
+const NavContent = styled.div<{ screenSize: string }>`
+  flex: 1;
+  justify-content: space-evenly;
+  align-items: center;
+  display: flex;
+  flex-flow: ${(props) => props.screenSize === "TABLET" ? "column wrap" : "row wrap"};
+  font-size: ${(props) => props.screenSize === "TABLET" ? "column wrap" : "row wrap"};
+`;
+
+
+const Navbar: React.FC<Props> = ({ screenSize }) => {
+
+  const navigationList = [
+    { title: "Reviews", link: "" },
+    { title: "For The Culture", link: "" },
+    { title: "The Courts", link: "" },
+    { title: "Fresh Cuts", link: "" },
+    { title: "The Classics", link: "" },
+    { title: "Search", link: "" },
+    { title: "Subscribe", link: "" },
+  ]
+
   return (
-    <>
+    <NavContainer>
       <TheDunKnowLogo size={"small"} />
-      <div>Navbar content</div>
-    </>
+      <NavContent screenSize={screenSize}>
+        {
+          navigationList.map(({ title }) => {
+            return <span>{title}</span>
+          })
+        }
+      </NavContent>
+      <span>BURGER ICON</span>
+    </NavContainer>
   )
 }
 
